99e97733eee20689bf3a7d4fe927dc183a0a08b1
likai <likai@neusoft.com>
2022-07-16 16:20:59 +0800

    抽出共通代码

--- src/views/ModuleView.vue
+++ src/views/ModuleView.vue
@@ -1,73 +1,15 @@
 <template>
-  <el-container ref="myContainer">
-    <template>
-      <el-backtop class="my_backtop"></el-backtop>
-    </template>
-    <el-aside>
-      <el-menu class="my_menu" @select="handleSelect"
-               :default-openeds="['0', '1','2','3','4','5','6','7','8','9','10']">
-        <template v-for="(item, primary) in list">
-          <el-menu-item
-            v-if="!item.submenu"
-            :key="primary"
-            :index="primary.toString()"
-          >
-            <span slot="title" :title="item.title">{{ item.title }} </span>
-          </el-menu-item>
-          <el-submenu v-else :key="primary" :index="primary.toString()">
-            <template slot="title">
-              <b>{{ item.title }}</b>
-            </template>
-            <el-menu-item
-              v-for="(subitem, subindex) in item.sublist"
-              :key="subitem"
-              :index="primary.toString() + '-' + subindex.toString()"
-            >
-              <span :title="subitem">
-                {{ subitem }}
-              </span>
-            </el-menu-item>
-          </el-submenu>
-        </template>
-      </el-menu>
-    </el-aside>
-    <!-- markdown 内容显示区域 -->
-    <el-main id="my_content">
-      <VueMarkdown
-        :source="content"
-        class="markdown-body"
-        id="root"
-        align="left"
-      />
-    </el-main>
-    <!-- 右侧边栏 目录 -->
-    <div class="my_catalog">
-      <side-catalog
-        class="catalog"
-        v-bind="catalogProps"
-        v-if="isShowCata"
-      ></side-catalog>
-    </div>
-    <div class="content">
-      <div class="backtop">
-        <el-backtop target=".content .backtop"></el-backtop>
-      </div>
-    </div>
-  </el-container>
+  <module></module>
 </template>
 
-<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
 <script>
-// import VueMarkdown from 'vue-markdown'
-import 'github-markdown-css'
-import 'highlight.js/styles/googlecode.css'
-import 'vue-side-catalog/lib/vue-side-catalog.css'
-// import SideCatalog from 'vue-side-catalog'
-import { highLightButton } from '@/js/util.js'
-import moduleJs from '@/js/module'
+import module from '@/components/MkdView'
+import { highLightButton } from '@/js/util'
 
 export default {
-  ...moduleJs,
+  components: {
+    module
+  },
   mounted() {
     highLightButton(1)
   }
@@ -75,5 +17,4 @@ export default {
 </script>
 
 <style scoped>
-@import "@/css/module.css";
 </style>
